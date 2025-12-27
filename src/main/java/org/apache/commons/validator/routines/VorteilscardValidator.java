7a8443fd62ded23e5de638663f88b73067d69ea6
homebeaver <homebeaver@outlook.de>
2025-12-27 11:39:49 +0100

    Vorteilscard offered by ÖBB, the Austrian national railway company

---
+++ src/main/java/org/apache/commons/validator/routines/VorteilscardValidator.java
@@ -0,0 +1,76 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.validator.routines;
+
+import java.io.Serializable;
+
+import org.apache.commons.validator.routines.checkdigit.LuhnCheckDigit;
+
+/**
+ * Vorteilscard number validation.
+ *
+ * <p>
+ * Vorteilscard is a discount subscription program offered by ÖBB, the Austrian national railway company.
+ * </p>
+ *
+ * <p>
+ * The card number is a 16-digit number. For example, it may start with 5047.
+ * </p>
+ *
+ * <p>
+ * For further information see
+ *  <a href="https://de.wikipedia.org/wiki/Vorteilscard">Wikipedia</a>.
+ * </p>
+ *
+ * @since 2.10.5
+ */
+public final class VorteilscardValidator implements Serializable {
+
+    private static final long serialVersionUID = 5459084341193160754L;
+
+	/** Singleton instance */
+    private static final VorteilscardValidator INSTANCE = new VorteilscardValidator();
+
+    /**
+     * Gets the singleton instance of this validator.
+     * @return A singleton instance of the card validator.
+     */
+    public static VorteilscardValidator getInstance() {
+        return INSTANCE;
+    }
+
+    private static final String FORMAT = "(5047)(\\d{12})";
+
+    static RegexValidator FORMAT_VALIDATOR = new RegexValidator(FORMAT);
+    static final CodeValidator VALIDATOR = new CodeValidator(FORMAT_VALIDATOR, 16, LuhnCheckDigit.getInstance());
+
+    /**
+     * Constructs a validator.
+     */
+    private VorteilscardValidator() { }
+
+    /**
+     * Tests whether the code is a valid card number.
+     *
+     * @param code The code to validate.
+     * @return {@code true} if a card number, otherwise {@code false}.
+     */
+    public boolean isValid(final String code) {
+        return VALIDATOR.isValid(code);
+    }
+
+}
--- src/test/java/org/apache/commons/validator/routines/checkdigit/LuhnCheckDigitTest.java
+++ src/test/java/org/apache/commons/validator/routines/checkdigit/LuhnCheckDigitTest.java
@@ -45,6 +45,7 @@ class LuhnCheckDigitTest extends AbstractCheckDigitTest {
             , VALID_IT_IVA_BANCA_ITALIA
             // Bahncard:
             , "7081410043891603", "7081411000000006", "7081420000000007"
+            , "5047000000000004" // ÖBB (Österreich) – Vorteilscard
             // Miles & More:
             , "999956789012347", "992056789012343", "992256789012349", "222000000000002", "333000000000008"
             // VATIN_SE:
