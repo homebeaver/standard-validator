4d6b21e22b510ad823467e64f610d0122486dc39
homebeaver <homebeaver@outlook.de>
2025-12-28 15:22:48 +0100

    BahnCardValidatorTest organize imports

--- src/test/java/org/apache/commons/validator/routines/BahnCardValidatorTest.java
+++ src/test/java/org/apache/commons/validator/routines/BahnCardValidatorTest.java
@@ -16,9 +16,7 @@
  */
 package org.apache.commons.validator.routines;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
