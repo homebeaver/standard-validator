84f62ee7b951978149cf5d84944f55e22e911a08
homebeaver <homebeaver@outlook.de>
2026-02-09 12:22:20 +0100

    Codice fiscale

--- src/main/java/org/apache/commons/validator/routines/checkdigit/TidITCheckDigit.java
+++ src/main/java/org/apache/commons/validator/routines/checkdigit/TidITCheckDigit.java
@@ -34,7 +34,7 @@ public final class TidITCheckDigit extends ModulusCheckDigit implements IsoIecCo
 
     private static final long serialVersionUID = -4719922002893417631L;
 
-	/** Singleton Check Digit instance */
+    /** Singleton Check Digit instance */
     private static final TidITCheckDigit INSTANCE = new TidITCheckDigit();
 
     /**
@@ -53,48 +53,31 @@ public final class TidITCheckDigit extends ModulusCheckDigit implements IsoIecCo
     }
 
 /*
-Si dividono in due gruppi i caratteri alfanumerici del codice fiscale: 
+Si dividono in due gruppi i caratteri ALPHANUMERIC del codice fiscale: 
 - in un gruppo vanno i caratteri la cui posizione all'interno del codice fiscale è dispari, 
 - nell'altro quelli la cui posizione è pari;
 
 fatto questo, i caratteri vengono convertiti in valori numerici secondo le seguenti tabelle;
-- posizione pari
-0:0 1:1 .. 9:9    01234 5  6  7  8  9  ABCDE F  G  H  I  J  K  L  M  N  O  P  Q  R  S  T  U  V  W  Y  X  Z
-                  01234 5  6  7  8  9  01234 5  6  7  8  9  10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
-- posizione dispari
-                  10579 13 15 17 19 21 10579 13 15 17 19 21 2  4  18 20 11 3  6  8  12 14 16 10 22 25 24 23
+- posizione PARI
+- posizione DISPARI
 
 a questo punto, tutti i valori ricavati con le tabelle sopra vanno sommati tra di loro 
-e il risultato va diviso per 26;     ==> summe MOD 26
+e il risultato va diviso per 26;
 il resto della divisione fornisce il codice identificativo, 
-ottenuto dalla seguente tabella di conversione:
-                                                                 10                           K
+ottenuto dalla ALPHABETIC tabella di conversione.
  */
-    private static final int[] DISPARI = { 1,0,5,7,9,13,15,17,19,21,1,0,5,7,9,13,15,17,19,21, 2, 4,18,20,11, 3, 6, 8,12,14,16,10,22,25,24,23 };
     private static final int[]    PARI = { 0,1,2,3,4, 5, 6, 7, 8, 9,0,1,2,3,4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25 };
-    // XXX ableiten von abstract class IsoIec7064PureSystem
-    // oder von IsoIecPure1271System - dort ist getCharacterSet ALPHANUMERIC
-    protected String getCharacterSet() {
-        return ALPHANUMERIC;
-    }
+    private static final int[] DISPARI = { 1,0,5,7,9,13,15,17,19,21,1,0,5,7,9,13,15,17,19,21, 2, 4,18,20,11, 3, 6, 8,12,14,16,10,22,25,24,23 };
 
     @Override
     protected int toInt(final char character, final int leftPos, final int rightPos) throws CheckDigitException {
-//        if (getCharacterSet().indexOf(character) == -1) {
-//            throw new CheckDigitException(CheckDigitException.invalidCharacter(character, leftPos));
-//        }
         int i = ALPHANUMERIC.indexOf(character);
         if (i == -1) {
             throw new CheckDigitException(CheckDigitException.invalidCharacter(character, leftPos));
         }
-        if (leftPos % 2 == 1) { // dispari
-            return DISPARI[i];
-        } else { // pari
-            return PARI[i];
-        }
+        return leftPos % 2 == 1 ? DISPARI[i] : PARI[i];
     }
-//    /** Weighting given to digits depending on their left position */
-//    private static final int[] POSITION_WEIGHT = { 2,7,9,1,4,6,3,5,8,2,7,9 };
+
     /**
      * The <i>weighted</i> value of a character in the the value of the character.
      *
@@ -105,26 +88,8 @@ ottenuto dalla seguente tabella di conversione:
      */
     @Override
     protected int weightedValue(int charValue, int leftPos, int rightPos) throws CheckDigitException {
-//        final int weight = POSITION_WEIGHT[(leftPos - 1) % POSITION_WEIGHT.length];
-//        return charValue * weight;
         return charValue;
     }
-
-//    protected int calculateModulus(final String code, final boolean includesCheckDigit) throws CheckDigitException {
-//        int total = 0;
-//        for (int i = 0; i < code.length(); i++) {
-//            final int lth = code.length() + (includesCheckDigit ? 0 : getCheckdigitLength());
-//            final int leftPos = i + 1;
-//            final int rightPos = lth - i;
-//            final int charValue = toInt(code.charAt(i), leftPos, rightPos);
-//            System.out.println("calculateModulus leftPos="+leftPos + "'"+code.charAt(i)+"' == "+charValue);
-//            total += weightedValue(charValue, leftPos, rightPos);
-//        }
-//        if (total == 0) {
-//            throw new CheckDigitException(CheckDigitException.ZERO_SUM);
-//        }
-//        return total % RADIX_26; // modulus; XXX
-//    }
 
     /**
      * {@inheritDoc}
