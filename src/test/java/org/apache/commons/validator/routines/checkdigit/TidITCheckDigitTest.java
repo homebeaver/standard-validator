24bbd918a51c7caa62327923d961db6e4c7af034
homebeaver <homebeaver@outlook.de>
2026-02-11 18:53:21 +0100

    organize imports

--- src/test/java/org/apache/commons/validator/routines/checkdigit/TidITCheckDigitTest.java
+++ src/test/java/org/apache/commons/validator/routines/checkdigit/TidITCheckDigitTest.java
@@ -16,9 +16,7 @@
  */
 package org.apache.commons.validator.routines.checkdigit;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
