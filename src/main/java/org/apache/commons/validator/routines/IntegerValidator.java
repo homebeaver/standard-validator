75cb976a04c9effa4339682bea7fe10286c32272
homebeaver <homebeaver@outlook.de>
2026-09-22 08:04:50 +0200

    sync with commons-validator : IntegerValidator (marginal)

--- src/main/java/org/apache/commons/validator/routines/IntegerValidator.java
+++ src/main/java/org/apache/commons/validator/routines/IntegerValidator.java
@@ -21,7 +21,7 @@ import java.text.NumberFormat;
 import java.util.Locale;
 
 /**
- * <p><strong>Integer Validation</strong> and Conversion routines ({{@link Integer}).</p>
+ * <strong>Integer Validation</strong> and Conversion routines ({{@link Integer}).
  *
  * <p>This validator provides a number of methods for
  *    validating/converting a {@link String} value to
@@ -70,6 +70,7 @@ public class IntegerValidator extends AbstractNumberValidator {
 
     /**
      * Gets the singleton instance of this validator.
+     *
      * @return A singleton instance of the IntegerValidator.
      */
     public static IntegerValidator getInstance() {
@@ -84,8 +85,8 @@ public class IntegerValidator extends AbstractNumberValidator {
     }
 
     /**
-     * <p>Construct an instance with the specified strict setting
-     *    and format type.</p>
+     * Construct an instance with the specified strict setting
+     *    and format type.
      *
      * <p>The {@code formatType} specified what type of
      *    {@code NumberFormat} is created - valid types
@@ -206,7 +207,7 @@ public class IntegerValidator extends AbstractNumberValidator {
     }
 
     /**
-     * <p>Validate/convert an {@code Integer} using the default
+     * Validate/convert an {@code Integer} using the default
      *    {@link Locale}.
      *
      * @param value The value validation is being performed on.
@@ -218,7 +219,7 @@ public class IntegerValidator extends AbstractNumberValidator {
     }
 
     /**
-     * <p>Validate/convert an {@code Integer} using the
+     * Validate/convert an {@code Integer} using the
      *    specified {@link Locale}.
      *
      * @param value The value validation is being performed on.
@@ -230,7 +231,7 @@ public class IntegerValidator extends AbstractNumberValidator {
     }
 
     /**
-     * <p>Validate/convert an {@code Integer} using the
+     * Validate/convert an {@code Integer} using the
      *    specified <em>pattern</em>.
      *
      * @param value The value validation is being performed on.
@@ -242,7 +243,7 @@ public class IntegerValidator extends AbstractNumberValidator {
     }
 
     /**
-     * <p>Validate/convert a {@code Integer} using the
+     * Validate/convert a {@code Integer} using the
      *    specified pattern and/ or {@link Locale}.
      *
      * @param value The value validation is being performed on.
